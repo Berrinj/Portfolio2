5a6035cccc392c0a3d8273ea64b98b588d7c6b0f
Berrinj <69366646+Berrinj@users.noreply.github.com>
2025-02-27 12:35:40 +0100

    sidebar populated with an array of skill badges

--- src/components/Layout/Sidebar/index.jsx
+++ src/components/Layout/Sidebar/index.jsx
@@ -1,7 +1,15 @@
+import { SkillsBadge } from "components/SkillsBadge";
+import { skills } from "data/skills";
+
 export const Sidebar = () => {
   return (
     <aside className="sidebar bg-selago w-1/12 h-auto">
       <p className="bg-firefly text-selago w-fit mx-auto">Skills</p>
+      <div className="skills-container px-4">
+        {skills.map((skill) => (
+          <SkillsBadge key={skill.id} skill={skill} />
+        ))}
+      </div>
     </aside>
   );
 };
