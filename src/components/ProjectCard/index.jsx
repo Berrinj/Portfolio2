bbddfb0b63f50a1c77565aced47f61119c5e34c1
Berrinj <69366646+Berrinj@users.noreply.github.com>
2025-03-01 19:58:06 +0100

    project card design and display

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -1,38 +1,129 @@
-import { IoIosExpand } from "react-icons/io";
+// import { IoIosExpand } from "react-icons/io";
+import { FaGithub } from "react-icons/fa";
+import { SiNetlify } from "react-icons/si";
+import styled from "styled-components";
+import { VscMultipleWindows } from "react-icons/vsc";
+// import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+const ProjectViewBtn = styled.a`
+  background-color: #f1e5fc;
+  display: flex;
+  align-items: center;
+  color: #000;
+  padding: 5px 5px;
+  text-align: center;
+  text-decoration: none;
+  gap: 5px;
+  box-shadow:
+    -2px -2px #e0dede,
+    -2px 0 #e0dede,
+    0 -2px #e0dede,
+    -4px -4px white,
+    -4px 0 white,
+    0 -4px white,
+    2px 2px #818181,
+    0 2px #818181,
+    2px 0 #818181,
+    2px -2px #e0dede,
+    -2px 2px #818181,
+    -4px 2px white,
+    -4px 4px black,
+    4px 4px black,
+    4px 0 black,
+    0 4px black,
+    2px -4px white,
+    4px -4px black;
+  margin: 10px;
+  cursor: pointer;
+  &:hover {
+    background-color: #dcd1e6;
+    color: #000;
+  }
+  &:active {
+    box-shadow:
+      -2px -2px #818181,
+      -2px 0 #818181,
+      0 -2px #818181,
+      -4px -4px black,
+      -4px 0 black,
+      0 -4px black,
+      2px 2px #e0dede,
+      0 2px #e0dede,
+      2px 0 #e0dede,
+      2px -2px #818181,
+      -2px 2px #e0dede,
+      -4px 2px black,
+      -4px 4px white,
+      4px 4px white,
+      4px 0 white,
+      0 4px white,
+      2px -4px black,
+      4px -4px white;
+  }
+`;
 
 export const ProjectCard = ({ project }) => {
-  const openProject = () => {
-    console.log("this will open the project page");
-  };
+  // const navigate = useNavigate();
 
+  // const openProject = () => {
+  //   console.log("this will open the project page");
+  //   navigate(`/projects/${project.id}`);
+  // };
   console.log(project);
 
   return (
     <>
-      <div className="project-card bg-white my-2 w-1/3">
-        <span className="bg-selago flex justify-between items-center p-2">
-          <h2>{project.title}</h2>
-          <IoIosExpand />
+      <div className="project-card my-2 min-w-[290px] w-9/10 sm:w-3/10 border-2 border-selago text-white flex flex-col">
+        <span className="flex justify-between items-center p-2">
+          <h2 className="text-xl">{project.title}</h2>
+          <VscMultipleWindows />
         </span>
         <img
           src={project.image}
           alt={project.title}
-          className="h-42 w-full object-cover"
+          className="aspect-3/2 object-cover border-y-2 border-selago"
         />
-        <div className="info px-4">
-          <p>View Project on:</p>
-          <div className="links flex justify-around">
-            <a href={project.netlifyUrl} target="_blank" rel="noreferrer">
-              Netlify
-            </a>
-            <a href={project.githubUrl} target="_blank" rel="noreferrer">
+        <div className="info p-4 grow flex flex-col justify-between">
+          <p>
+            <span className="bg-selago text-firefly">About:</span>
+            {""} {project.description}
+          </p>
+        </div>
+        <div className="bg-selago w-full h-auto">
+          <div className="links h-14 flex items-center flex-wrap">
+            <Link to={`/projects/${project.id}`}>
+              <button
+                className="bg-selago text-firefly hover:rounded-none hover:bg-white hover:text-firefly win-btn p-1 mx-3"
+                // onClick={openProject}
+              >
+                <VscMultipleWindows />
+              </button>
+            </Link>
+            <div className="line h-full bg-firefly"></div>
+            <ProjectViewBtn
+              href={project.netlifyUrl}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <SiNetlify /> Netlify
+            </ProjectViewBtn>
+            <ProjectViewBtn
+              href={project.githubUrl}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaGithub />
               GitHub
-            </a>
+            </ProjectViewBtn>
           </div>
         </div>
-        <button className="w-full" onClick={openProject}>
+        {/* <button
+          className="w-full bg-selago text-firefly hover:rounded-none hover:bg-white hover:text-firefly win-btn"
+          onClick={openProject}
+        >
           See More
-        </button>
+        </button> */}
       </div>
     </>
   );
