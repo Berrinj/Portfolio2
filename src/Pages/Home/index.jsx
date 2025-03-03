8a184e1d5255dab8eb92894809cd20238ae3c312
Berrinj <69366646+Berrinj@users.noreply.github.com>
2025-03-03 21:26:06 +0100

    Home page upadted with less about mee on default, and hopefully i will filter and render my projectcards based on the fiter ctaegories in the projects array  later this week.

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,28 +1,53 @@
 import { ProjectCard } from "components/ProjectCard";
 import { Projects } from "data/Projects";
+import { VscMultipleWindows } from "react-icons/vsc";
 
 function Home() {
   return (
     <div className="home h-full min-h-96 p-3">
-      <h1 className="text-white min-h-1/2 text-center">Home</h1>
-      <div className="about flex flex-row-reverse p-3 justify-between">
-        <div className="bg-selago w-2/6 h-fit p-2 flex justify-center items-center text-firefly relative">
-          <p>About</p>
-          <p className="absolute right-2 bottom-0 italic text-sm">Les mer..</p>
-        </div>
+      <div className="about flex flex-row p-3 justify-between">
+        <img
+          src="src/assets/images/profilbilde-300.png"
+          alt="Marte"
+          className="sm:hidden w-32 h-auto object-contain border-2 border-selago me-2"
+        />
         <p className="w-4/6 text-white pe-2">
-          34 år gammel kvinne, bor i Trondheim men kommer opprinnelig fra Grong.
-          Har en lang karriere bak meg fra barnehage, men har nå tatt steget
-          videre til å bli Front-end Developer. Uteksaminert som Front-end
-          Development fra Noroff Fagskole mars 2025.
+          34 år gammel, bor i Trondheim men kommer opprinnelig fra Grong. Har en
+          lang karriere bak meg fra barnehage, men har nå utdannet meg som
+          Front-end Developer.
         </p>
+        <div className="bg-selago hidden sm:w-2/6 h-fit p-2 sm:flex justify-center items-center text-firefly relative win-btn">
+          <p className="text-xl">Om</p>
+          <p className="absolute hidden sm:block right-2 bottom-0 italic text-sm">
+            Les mer..
+          </p>
+        </div>
+        <div className="more-btn win-btn h-8 w-8 flex justify-center items-center sm:hidden">
+          <VscMultipleWindows />{" "}
+        </div>
       </div>
-      <div className="projects">
-        <p>Projects</p>
-        <div className="project-cards flex justify-center gap-5">
-          {Projects.map((project, index) => (
-            <ProjectCard key={index} project={project} />
-          ))}
+      <div className="projects border-2 border-selago">
+        <div className="bg-selago h-fit min-w-[250px] w-fit p-2 flex justify-center items-center text-firefly">
+          <p className="text-xl">Prosjekter: </p>
+          <div className="filter">
+            <select
+              name="filter"
+              id="filter"
+              className="text-center font-semibold"
+            >
+              <option value="all">Alle</option>
+              <option value="school">Skole</option>
+              <option value="fun">må prosjekter</option>
+              <option value="personal">Personal</option>
+            </select>
+          </div>
+        </div>
+        <div className="cards-container flex mx-auto justify-center">
+          <div className="project-cards flex flex-wrap gap-3 justify-center">
+            {Projects.map((project, index) => (
+              <ProjectCard key={index} project={project} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
