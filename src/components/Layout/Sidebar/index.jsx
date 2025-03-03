9846a2e88d49d8c88bec1e020d5e9c9430dc13d6
Berrinj <69366646+Berrinj@users.noreply.github.com>
2025-03-03 21:23:56 +0100

    upadted mapping after skills was updated/upgraded

--- src/components/Layout/Sidebar/index.jsx
+++ src/components/Layout/Sidebar/index.jsx
@@ -1,12 +1,17 @@
 import { SkillsBadge } from "components/SkillsBadge";
 import { skills } from "data/skills";
 
+const convertSkillsToArray = () => {
+  return Object.values(skills);
+};
+
 export const Sidebar = () => {
+  const skillsArray = convertSkillsToArray();
   return (
-    <aside className="sidebar bg-selago w-1/12 h-auto">
+    <aside className="sidebar bg-selago min-w[100px] h-auto hidden md:block">
       <p className="bg-firefly text-selago w-fit mx-auto">Skills</p>
-      <div className="skills-container px-4">
-        {skills.map((skill) => (
+      <div className="skills-container px-4 w-full">
+        {skillsArray.map((skill) => (
           <SkillsBadge key={skill.id} skill={skill} />
         ))}
       </div>
