39dfb82fb35e2ed5553ec893d4d66cba9eef6074
Berrinj <69366646+Berrinj@users.noreply.github.com>
2025-03-05 20:49:16 +0100

    badge wont show if there is no image assosiated with the skill

--- src/components/Layout/Sidebar/index.jsx
+++ src/components/Layout/Sidebar/index.jsx
@@ -11,9 +11,10 @@ export const Sidebar = () => {
     <aside className="sidebar bg-selago min-w[100px] h-auto hidden md:block">
       <p className="bg-firefly text-selago w-fit mx-auto">Skills</p>
       <div className="skills-container px-4 w-full">
-        {skillsArray.map((skill) => (
-          <SkillsBadge key={skill.id} skill={skill} />
-        ))}
+        {skillsArray.map(
+          (skill) =>
+            skill.image && <SkillsBadge key={skill.id} skill={skill} />,
+        )}
       </div>
     </aside>
   );
