48b2f13333cf1c358e5de4379f86133183fb9da3
Berrinj <69366646+Berrinj@users.noreply.github.com>
2025-03-06 12:18:03 +0100

    changes changes changes, paragraps on the data retrived and several UX/UI updates

--- src/Pages/Project/index.jsx
+++ src/Pages/Project/index.jsx
@@ -12,6 +12,8 @@ export const Project = () => {
     return <div>Project not found</div>;
   }
 
+  const paragraphs = project.description.split("\n\n");
+
   return (
     <div className="project-main p-3">
       <div className="project-details border-2 border-selago">
@@ -25,47 +27,84 @@ export const Project = () => {
             </div>
           </Link>
         </div>
-        <img src={project.image} alt={project.title} />
-        <div className="project-title text-center">
-          <h1 className="text-3xl">{project.title}</h1>
-          {project.subtitle && <h2 className="text-xl">{project.subtitle}</h2>}
-        </div>
-        <p>{project.description}</p>
-        <p>Technologies Used:</p>
-        {project.skills && project.skills.length > 0 ? (
-          <ul className="flex gap-2">
-            {project.skills.map(
-              (tech, index) =>
-                tech && (
-                  <li
-                    key={tech.id || index}
-                    className="flex items-center gap-2"
-                  >
-                    {tech.image ? (
-                      <span className="flex gap-1">
-                        <img
-                          src={tech.image}
-                          alt={tech.name}
-                          className="w-6 h-6"
-                        />
-                        {tech.name}
-                      </span>
-                    ) : (
-                      <span>{tech.name}</span>
-                    )}
-                  </li>
-                ),
+        <img
+          src={project.image}
+          alt={project.title}
+          className="border-b border-selago"
+        />
+        <div className="about-project p-3">
+          <div className="project-title text-center">
+            <h1 className="text-3xl">{project.title}</h1>
+            {project.subtitle && (
+              <h2 className="text-xl">{project.subtitle}</h2>
             )}
-          </ul>
-        ) : (
-          <p>No skills listed for this project.</p>
-        )}
-        <a href={project.netlifyUrl} target="_blank" rel="noreferrer">
-          View on Netlify
-        </a>
-        <a href={project.githubUrl} target="_blank" rel="noreferrer">
-          View on GitHub
-        </a>
+          </div>
+          <div className="project-description py-3 ">
+            <h3 className="text-xl border border-selago px-1 bg-selago/10">
+              Om prosjektet:
+            </h3>
+            {paragraphs.map((paragraph, index) => (
+              <p key={index} className="p-1">
+                {paragraph}
+              </p>
+            ))}
+            {project.additonalInfo && (
+              <p className="italic text-selago p-1 m-5 bg-selago/10">
+                {project.additonalInfo}
+              </p>
+            )}
+          </div>
+          <p className="border border-selago px-1 bg-selago/10">
+            Technologies Used:
+          </p>
+          {project.skills && project.skills.length > 0 ? (
+            <ul className="flex flex-wrap gap-2 p-1 mt-2">
+              {project.skills.map(
+                (tech, index) =>
+                  tech && (
+                    <li
+                      key={tech.id || index}
+                      className="flex items-center gap-2 p-1"
+                    >
+                      {tech.image ? (
+                        <span className="flex gap-1 text-sm">
+                          <img
+                            src={tech.image}
+                            alt={tech.name}
+                            className="w-4 h-4"
+                            aria-label={`${tech.name} logo`}
+                          />
+                          {tech.name}
+                        </span>
+                      ) : (
+                        <span>{tech.name}</span>
+                      )}
+                    </li>
+                  ),
+              )}
+            </ul>
+          ) : (
+            <p>No skills listed for this project.</p>
+          )}
+        </div>
+        <div className="buttons flex flex-wrap justify-around mt-3 bg-selago p-2 text-firefly">
+          <a
+            href={project.netlifyUrl}
+            target="_blank"
+            rel="noreferrer"
+            className="win-btn p-2"
+          >
+            View on Netlify
+          </a>
+          <a
+            href={project.githubUrl}
+            target="_blank"
+            rel="noreferrer"
+            className="win-btn p-2"
+          >
+            View on GitHub
+          </a>
+        </div>
       </div>
     </div>
   );
