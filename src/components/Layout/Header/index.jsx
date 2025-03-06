0f652ad7d572d30844e2e73ef6c3fe2e2469ac79
Berrinj <69366646+Berrinj@users.noreply.github.com>
2025-03-06 13:20:07 +0100

    upadted header for fixed navbar

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <div className="header h-46 md:h-64 sm:bg-[url(assets/images/headerimg.png)] bg-contain bg-no-repeat bg-right sm:border-e-2 border-t-2 border-selago ">
+    <div className="header h-46 md:h-64 sm:bg-[url(assets/images/headerimg.png)] bg-contain bg-no-repeat bg-right sm:border-e-2 border-t-2 border-selago mt-10 sm:mt-0">
       <div className="header--text-container w-full sm:w-9/11 lg:w-6/7 bg-selago uppercase clip-right-angle h-full relative">
         <p className="bg-firefly text-selago w-fit font-thin absolute top-0 right-0 sm:left-0">
           Front-end Utvikler
@@ -15,9 +15,6 @@ export const Header = () => {
           </h1>
         </div>
       </div>
-      {/* <div className="header--img-container border-t-2 border-e-2 border-selago w-1/4 clip-left-angle">
-        <img src={headerbackground} />
-      </div> */}
     </div>
   );
 };
