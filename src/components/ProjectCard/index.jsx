2ae1a5bc3559504dc766f96a574eae85ae5b0a4e
Berrinj <69366646+Berrinj@users.noreply.github.com>
2025-03-09 20:53:41 +0100

    removed styled button and used class for button consistency

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -1,66 +1,8 @@
 // import { IoIosExpand } from "react-icons/io";
 import { FaGithub } from "react-icons/fa";
 import { SiNetlify } from "react-icons/si";
-import styled from "styled-components";
 import { VscMultipleWindows } from "react-icons/vsc";
 import { Link } from "react-router-dom";
-
-const ProjectViewBtn = styled.a`
-  background-color: #f1e5fc;
-  display: flex;
-  align-items: center;
-  color: #000;
-  padding: 5px 5px;
-  text-align: center;
-  text-decoration: none;
-  gap: 5px;
-  box-shadow:
-    -2px -2px #e0dede,
-    -2px 0 #e0dede,
-    0 -2px #e0dede,
-    -4px -4px white,
-    -4px 0 white,
-    0 -4px white,
-    2px 2px #818181,
-    0 2px #818181,
-    2px 0 #818181,
-    2px -2px #e0dede,
-    -2px 2px #818181,
-    -4px 2px white,
-    -4px 4px black,
-    4px 4px black,
-    4px 0 black,
-    0 4px black,
-    2px -4px white,
-    4px -4px black;
-  margin: 10px;
-  cursor: pointer;
-  &:hover {
-    background-color: #dcd1e6;
-    color: #000;
-  }
-  &:active {
-    box-shadow:
-      -2px -2px #818181,
-      -2px 0 #818181,
-      0 -2px #818181,
-      -4px -4px black,
-      -4px 0 black,
-      0 -4px black,
-      2px 2px #e0dede,
-      0 2px #e0dede,
-      2px 0 #e0dede,
-      2px -2px #818181,
-      -2px 2px #e0dede,
-      -4px 2px black,
-      -4px 4px white,
-      4px 4px white,
-      4px 0 white,
-      0 4px white,
-      2px -4px black,
-      4px -4px white;
-  }
-`;
 
 export const ProjectCard = ({ project }) => {
   return (
@@ -82,16 +24,16 @@ export const ProjectCard = ({ project }) => {
           alt={project.title}
           className="aspect-3/2 object-cover border-y-2 border-selago"
         />
-        <div className="info p-4 grow flex flex-col justify-between">
+        <div className="info p-4 grow flex flex-col justify-between text-white">
           <p>
-            <span className="bg-selago/10 text-selago border border-selago">
+            <span className="bg-selago/10 border border-selago">
               Om prosjektet:
             </span>
             {""} {project.shortDescription}
           </p>
         </div>
         <div className="bg-selago w-full h-auto">
-          <div className="links h-14 flex items-center flex-wrap">
+          <div className="links h-14 flex items-center flex-wrap text-firefly">
             <Link to={`/projects/${project.id}`} aria-label="View Project">
               <button
                 className="bg-selago text-firefly hover:rounded-none hover:text-firefly win-btn p-1 mx-3"
@@ -101,23 +43,25 @@ export const ProjectCard = ({ project }) => {
               </button>
             </Link>
             <div className="line h-full bg-firefly"></div>
-            <ProjectViewBtn
+            <a
               href={project.netlifyUrl}
               target="_blank"
               rel="noreferrer"
               aria-label="View on Netlify"
+              className="win-btn p-1 mx-3 flex items-center gap-1"
             >
               <SiNetlify /> Netlify
-            </ProjectViewBtn>
-            <ProjectViewBtn
+            </a>
+            <a
               href={project.githubUrl}
               target="_blank"
               rel="noreferrer"
               aria-label="View GitHub repo"
+              className="win-btn p-1 mx-3 flex items-center gap-1"
             >
               <FaGithub />
               GitHub
-            </ProjectViewBtn>
+            </a>
           </div>
         </div>
       </div>
